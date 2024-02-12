b80f874c7885633554f7d0c5968dcad38d8911b0
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-02-12 15:42:46 +0530

    feat/countObject added

--- src/countObjects.java
+++ src/countObjects.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class countObjects {
+public  class countObjects {
+    static int count=0;
+    countObjects(){
+        count++;
+    }
+    static int getCount(){
+        return count;
+    }
+
+    public static void main(String[] args) {
+        countObjects obj1 = new countObjects();
+        countObjects obj2 = new countObjects();
+        countObjects obj3 = new countObjects();
+        System.out.println("count: "+countObjects.getCount());
+    }
 }
